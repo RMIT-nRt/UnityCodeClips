b10e2720618b937edbf4ba014d76993f953702c0
RMIT-nRt <eneti.waretini@rmit.edu.au>
2024-11-26 14:19:57 +1100

    Add files via upload

---
+++ Player Controllers/PlayerGridMovement.cs
@@ -0,0 +1,185 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGridMovement : MonoBehaviour
+{
+    //SCRIPT OVERVIEW: ================================================================================================
+    //This allows the player to move a specified gridSize, or "step" size, at a regular interval (walkDelay).
+    //The GridMovement function deals with the input (both arrow keys and WASD) then sends what direction is
+    //pressed to a Coroutine that delays the movement, or "steps", by the interval.
+
+    //OPTIONAL: A "BodyRotating" has been added if you need to rotate the player sprite in the direction they
+    //are moving. If this is required then the player sprite image gameObject MUST be a child gameObject of this
+    //player controller and assigned to the "bodySprite" variable. 
+
+
+    //Create varible (rb) for rigidbody
+    Rigidbody2D rb;
+
+    //Create varible to store last direction pressed
+    Vector2 newPos;
+
+    //Create a bool (true/false variable) to store if player is already moving
+    bool moving;
+
+    //Create a variable that you can edit in the Unity editor (SerializeField)
+    //that will set the gridSize or the size of the "step" the player will take each keypress
+    [SerializeField] float gridSize = 1;
+
+    //Create a variable that will be the speed of player moving from "step" to "step"
+    [SerializeField] float walkSpeed = 40;
+
+    //Create a varible which will be the time between each of those "steps"
+    [SerializeField] float walkDelay = 0.3f;
+
+    //OPTIONAL: Create Transform variable for the body sprite gameObject
+    [SerializeField] Transform bodySprite;
+
+
+    void Start()
+    {
+        //Assign rigidbody to rb variable
+        rb = GetComponent<Rigidbody2D>();
+
+        //Assign the newPos vector to where the player is at the start
+        newPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        //Run custom movement method/function
+        GridMovement();
+    }
+
+
+    //Custom movement method/function ====================================================================================
+    void GridMovement()
+    {
+        //If the player isn't already moving...
+        //!moving is the same as "moving == false", where as,
+        //moving (without the exclamation) is the same as "moving == true"
+        if (!moving)
+        {
+
+            //..check to see if any of the direction keys are being pressed...
+            //Making sure to check for both the arrow keys OR the WASD keys.
+            //Note: "OR" is defined by the double "pipes" (||) located with the "back-slash" key
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                //Then start the coroutine (an in-built method that deals with "timed" events)
+                //and state what direction was pressed ("left").
+                StartCoroutine(MovementDelay("left"));
+
+                //Optional: if needing player sprite to rotate in direction of movement
+                BodyRotating("left");
+            }
+
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                StartCoroutine(MovementDelay("right"));
+                BodyRotating("right");
+            }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                StartCoroutine(MovementDelay("up"));
+                BodyRotating("up");
+            }
+
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                StartCoroutine(MovementDelay("down"));
+                BodyRotating("down");
+            }
+        }
+
+        //Move the rigidbody (rb.MovePosition) using the Vector3.MoveTowards functionfrom the
+        //Players current position (transform.position) to the new position (newPos) at the
+        //speed, "walkSpeed" multiplied by time rather then framerate
+        rb.MovePosition(Vector3.MoveTowards(transform.position, newPos, walkSpeed * Time.deltaTime));
+    }
+
+
+    //Coroutine function (Or "IEnumerator) ===============================================================================
+    //These are great for dealing with "timed" events (note the "WaitForSeconds" function).
+    //Also note a string variable (dir) was added in the brackets of the coroutine so we code state
+    //the direction pressed when we started the coroutines above.
+    IEnumerator MovementDelay(string dir)
+    {
+        //First state that Player is moving so to ignore any other keys pressed
+        moving = true;
+
+        //A "switch" is like a series of "if" statements.
+        //Feed whatever changing variable we are going to check (dir) into the brackets next to "switch".
+        //Then state what each of the possiblities are for that changing varible ( "case" then the possibility).
+        //Then what action to take incase of that possibility, ensuring to add a "break" after that action.
+        switch (dir)
+        {
+            //If coroutine is start with a "left" string added to the coroutine (see above at line 61)...
+            case "left":
+
+                //then update the newPos vector with wherever the Player is current located
+                //plus the gridSize (or "step" size) in the left direction.
+                //Repeat this for all other directions
+                newPos = transform.position + new Vector3(-gridSize, 0);
+                break;
+            case "right":
+                newPos = transform.position + new Vector3(gridSize, 0);
+                break;
+            case "up":
+                newPos = transform.position + new Vector3(0, gridSize);
+                break;
+            case "down":
+                newPos = transform.position + new Vector3(0, -gridSize);
+                break;
+        }
+
+        //then add a delay in seconds (walkDelay)..
+        yield return new WaitForSeconds(walkDelay);
+
+        // before setting "moving" to false allowing another key/direction press
+        moving = false;
+    }
+
+
+    //Custom body rotate method/function =================================================================================
+    //NOTE: You can add a string variable within the brackets of the method which means
+    //a string MUST be added when calling the method/function (see line 77). This vartiable will
+    //then be available within the method/function
+    void BodyRotating(string dir)
+    {
+        switch (dir)
+        {
+            //If method/function is called with the "dir" string set to "left"
+            //then rotate the bodySprtie to the appropriate rotation.
+            //REMINDER: The rotation amounts below are relative to the body first facing upwards.
+            case "left":
+                bodySprite.localRotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case "right":
+                bodySprite.localRotation = Quaternion.Euler(0, 0, -90);
+                break;
+            case "up":
+                bodySprite.localRotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case "down":
+                bodySprite.localRotation = Quaternion.Euler(0, 0, 180);
+                break;
+        }
+    }
+
+
+    //Collision Trigger Event ===========================================================+===============================
+    //If portals are in your game will need the following code to reset
+    //the newPos variable to the second portal position
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Portal")
+        {
+            newPos = transform.position;
+        }
+    }
+}
---
+++ Player Controllers/PlayerStrictMovement.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStrictMovement : MonoBehaviour
+{
+    //SCRIPT OVERVIEW: =============================================================================
+    //A simple movement script that only allows player to move in a single direction, either vertical
+    //or horizontal and not diagonally. We then apply this to the velocity of the rigidbody.
+
+    //OPTIONAL: A "BodyRotating" has been added if you need to rotate the player sprite in the direction they
+    //are moving. If this is required then the player sprite image gameObject MUST be a child gameObject of this
+    //player controllerand assigned to the "bodySprite" variable
+
+
+    //Create varible (rb) for rigidbody
+    Rigidbody2D rb;
+
+    //Create varibles for horizontal & vertical inputs
+    float hInput, vInput;
+
+    //Create varible to store last direction pressed
+    Vector2 lastDir;
+
+    //Create variable for walkSpeed that can be adjusted in the Unity Editor (SerializeField)
+    [SerializeField] float walkSpeed = 3;
+
+    //OPTIONAL: Create Transform variable for the body sprite gameObject.
+    [SerializeField] Transform bodySprite;
+
+
+    void Start()
+    {
+        //Assign rigidbody to rb variable
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        //Run custom movement method/function
+        StrictMovement();
+
+        //Run custom body rotation method/function
+        BodyRotation();
+    }
+
+
+    //Custom movement method/function =============================================================
+    void StrictMovement()
+    {
+        //Assign axises to varible names.
+        //"GetAxis" will work for both the arrow keys & WASD as well as joysticks.
+        //"GetAxis" will give a number between -1 & 1,
+        //where -1 will be left (or down for vertical axis)
+        //and 1 will be right (or up for vertical axis).
+        //When either direction is realeased the axis will return to 0.
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        //Assign each axis varible to independent vectors
+        Vector2 hDir = new Vector2(hInput, 0);
+        Vector2 vDir = new Vector2(0, vInput);
+
+
+        //If left or right (A/D) pressed store in lastDir variable
+        //"Or" is defined by the double "pipes" (||) located with the "back-slash" key
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            lastDir = hDir;
+        }
+
+        //If up or down (W/S) pressed store in lastDir variable
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            lastDir = vDir;
+        }
+
+
+        //If any direction released set lastDir to zero
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) ||
+            Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) ||
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            lastDir = Vector2.zero;
+        }
+
+
+        //Apply lastDir to rigidbody multiplied by walkSpeed
+        rb.velocity = (lastDir * walkSpeed);
+    }
+
+
+    //Custom body rotation method/function =================================================================================
+    void BodyRotation()
+    {
+        //If the direction keys are pressed (arrow keys or WASD) then set the appropriate rotation
+        //REMINDER: The rotation amounts below are relative to the body first facing upwards.
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            bodySprite.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            bodySprite.localRotation = Quaternion.Euler(0, 0, -90);
+        }
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            bodySprite.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            bodySprite.localRotation = Quaternion.Euler(0, 0, 180);
+        }
+    }
+}
