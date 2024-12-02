b0d66b62ecb02590afd1a23e63736049b9b86b7b
RMIT-nRt <eneti.waretini@rmit.edu.au>
2024-12-02 19:02:36 +1100

    Add files via upload

---
+++ 5. Enemy Scripts/EnemyDamager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamager : MonoBehaviour
+{
+    //SCRIPT OVERVIEW: =============================================================================
+    //A simple script that causes damge to the player when it comes in contact with the enemy.
+    //It relies on the creation of the health script and must be linked in the editor.
+    //It will also apply force (pushBackForce) to the player and push the player away for a
+    //set time (pushBackTime)
+
+    //OPTIONAL: Camera shake has been add as an optional feature so relies on the CameraShake
+    //script being on the camera and linked in the editor also.
+
+    //Create a variable to link the Health script
+    [SerializeField] Health health;
+
+    //Create a variable to link the CameraShake script
+    [SerializeField] CameraShake cam;
+
+    //Create a varible to store the force applied to the player if it touches an enemy
+    [SerializeField] float pushBackForce = 4;
+
+    //Create a varible to store the time the force is applied to the player if it touches an enemy
+    [SerializeField] float pushBackTime = 0.5f;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the the enemy enters into the trigger of a gameObject with the tag, "Player"...
+        //REMEMBER: "isTrigger" must be ticked on the enemy's collider.
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Access the Health script and remove -1 health and update health display
+            health.UpdateHealth();
+
+            //Access the CameraShake script and start shake
+            if (cam != null)
+            {
+                cam.StartShake();
+            }
+
+            //Calculate push back direction which is just the player (collision object) position minus the enemies position
+            Vector3 pushDir = collision.transform.position - transform.position;
+
+            //Turn of the Players Basic Movement script so push back force can be applied.
+            //NOTE: below requires the PlayerBasicMovement script but this can be whatever your movement script is called.
+            collision.GetComponent<PlayerBasicMovement>().enabled = false;
+
+            //Apply force to players rigidbody in the pushDir multiplied by pushBackForce
+            collision.GetComponent<Rigidbody2D>().velocity = pushDir * pushBackForce;
+
+            //Start the push timer coroutine and a the collision object (player) into the brackets (see below)
+            StartCoroutine(PushTimer(collision.gameObject));
+        }
+    }
+
+
+    //IEnumerators or "coroutines" are great for timed events. See line 65 below where we can delay an action in seconds
+    //NOTE: A GameObject variable has been added in the brackets so we can have access to the player gameObject
+    IEnumerator PushTimer(GameObject player)
+    {
+        //Wait for pushBackTime...
+        yield return new WaitForSeconds(pushBackTime);
+
+        ///...then turn the players movement script back on.
+        /////NOTE: below requires the PlayerBasicMovement script but this can be whatever your movement script is called.
+        player.GetComponent<PlayerBasicMovement>().enabled = true;
+    }
+}
--- 5. Enemy Scripts/WaypointMovement.cs
+++ 5. Enemy Scripts/WaypointMovement.cs
@@ -4,34 +4,98 @@ using UnityEngine;
 
 public class WaypointMovement : MonoBehaviour
 {
-    //Create an array of waypoints and serialize field so we
-    //can drag the objects into the array in the editor
-    [SerializeField] Transform[] wayPoints;
+    //SCRIPT OVERVIEW: =============================================================================
+    //A simple patrolling script for enemies but can be used on platforms too.
+    //The gamObject will move between any number of positions (waypoints) and
+    //pause at each as desired (pauseTime) in seconds.
 
-    //Create an 'index' to select which waypoint, in the array,
-    //we are moving towards
-    int currentWaypoint = 0;
+    //NOTE: For this script to function correctly, create 3+ gameObjects underneath
+    //an empty gameObject. The first one will be the enemy/platform which holds this script as well
+    //as the enemy/platform sprite renderer, collider etc.
+    //The other gameObjects will be the "waypoints" or positions the enemy/platform will move
+    //and only need to be placed in the desired positions and require nothing else.
 
-    //Speed at which to move
+
+    //Create an array[] of Transform variables.
+    //An array contains as many of the same variable types as you wish and can be access
+    //using an interger within the square brackets e.g. waypoints[1]
+    //NOTE: An array is "zero-indexed" meaning the first one is index 0, second index 1 etc.
+    [SerializeField] Transform[] waypoints;
+
+    //Create a interger variable to store the current index for the waypoints delay
+    int waypointIndex = 0;
+
+    //Create a float to store the speed and another for the pause time when the enemy/platform
+    //reaches that position. REMINDER: pauseTime is in seconds
     [SerializeField] float speed = 2f;
+    [SerializeField] float pauseTime = 1;
 
+    //Create a bool variable to store whether enemy/platform is "pausing".
+    bool pause = false;
+
+    //Create a varible to store the current target position the enemy/platform is moving towards
+    Transform currentTarget;
+
+
+    void Start()
+    {
+        //Set currecTarget to first waypoint. REMEMBER the array is "zero-indexed".
+        currentTarget = waypoints[0];
+    }
 
 
     void Update()
     {
-        // Move to current waypoint using Vector3.MoveTowards(current position, target position, movement delta)
-        transform.position = Vector3.MoveTowards(transform.position, wayPoints[currentWaypoint].position, speed * Time.deltaTime);
+        //Run custom waypoint moving method/function
+        WaypointMove();
+    }
 
-        //Calculate how close to current waypoint
-        float distance = Vector2.Distance(transform.position, wayPoints[currentWaypoint].position);
 
-        //If really close to current waypoint then change waypoint 'index' to next one
-        if (distance < 0.1f)
+    void WaypointMove()
+    {
+        //Check to see if the enemy/platform ISN'T in a pause state...
+        if (!pause)
         {
-            currentWaypoint++;
+            //and if not paused, move the enemy/platform toward the current target
+            transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
+        }
+        
 
-            //Ensure to loop back to the first waypoint after the last one
-            currentWaypoint %= wayPoints.Length;
+        //Measure how close you are to target position and if extremely close (<0.1f) then...
+        if (Vector2.Distance(transform.position, currentTarget.position) < 0.1f)
+        {
+            //Increase the waypointIndex by one.
+            //NOTE: waypointIndex++ is the same as write waypointIndex+=1 or waypointIndex = waypointIndex + 1;
+            waypointIndex++;
+
+            //Using modulo (%) we can loop the waypointIndex between 0 and the number of waypoints (.length) in the array.
+            waypointIndex %= waypoints.Length;
+
+            //Update the currentTarget to the new position in the waypoints array
+            currentTarget = waypoints[waypointIndex];
+
+            //If enemy/platform movement is not in a pause state
+            if (!pause)
+            {
+                //Start pause coroutine (below)
+                StartCoroutine(Pause());
+            }
         }
     }
+
+
+    //IEnumerators or "coroutines" are great for timed events. See line 91 below where we can delay an action in seconds
+    IEnumerator Pause()
+    {
+        //Set enemy/platform to pause state...
+        pause = true;
+
+        //...wait pauseTie...
+        yield return new WaitForSeconds(pauseTime);
+
+        //...then unpause.
+        pause = false;
+    }
 }
+
+
