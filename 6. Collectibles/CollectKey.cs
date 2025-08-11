c18d5df7ac0125326df75f24b6ac6d1b5b530c3e
RMIT-nRt <eneti.waretini@rmit.edu.au>
2025-08-11 12:12:29 +1000

    Update CollectKey.cs

--- 6. Collectibles/CollectKey.cs
+++ 6. Collectibles/CollectKey.cs
@@ -4,28 +4,14 @@ using UnityEngine;
 
 public class collectKey : MonoBehaviour
 {
-    // super simple one key for one dooor
+    // Super simple one key for one dooor
     
-    [SerializeField] Collider2D Block;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // reset on game restart
-     
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Collider2D Door; //Ensure your door object has a 2D Collider
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            
             Block.enabled = false;
             Destroy(Block.gameObject);
 
